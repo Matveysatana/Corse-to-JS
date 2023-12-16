85f9dfdfba9e6e72c89ba658d9dd69415e441f57
Matvey <example@gmail.com>
2023-12-16 11:16:16 +0300

    Coure to JS

---
+++ First.js
@@ -0,0 +1,36 @@
+//  alert("Hello World");
+//  просто выведение окна
+
+
+//  let answer = confirm(" How are you?");
+//  console.log(answer);
+// //  Окно с вопросом
+
+ let answer = prompt(" You are a chiled?", "yes")
+ console.log(answer);
+
+ let incr = 10,
+    decr = 10;
+
+console.log(incr--);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);//Сравнивает по значению
+console.log("2" === 2);//Сравнивает по типу данных
+
+let isCheced = true,
+    isClose = false;
+console.log( isCheced && isClose);
+
+
+
+
+
+
+
+
+
+
+
+
